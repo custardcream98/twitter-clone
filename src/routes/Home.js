266c6b8658974ec34ba3dd657981cfc4d508dedb
custardcream@kakao.com <custardcream@kakao.com>
2022-07-10 22:41:17 +0900

    Firestore 이용해서 트윗 올리기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,43 @@
 import React from "react";
+import { useState } from "react";
+import { dbService } from "firebaseInstance";
+import { collection, addDoc } from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [cweet, setCweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let doc = await addDoc(collection(dbService, "cweets"), {
+        cweet: cweet,
+        createdAt: Date.now(),
+      });
+      console.log(`${doc}문서 생성~!`);
+    } catch (e) {
+      console.log(e.message);
+    }
+    setCweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setCweet(value);
+  };
+  return (
+    <div>
+      <form>
+        <input
+          value={cweet}
+          type="text"
+          palceholder="트윗 내용을 입력해라"
+          maxLength={120}
+          onChange={onChange}
+        ></input>
+        <input type="submit" value="Cwitt" onClick={onSubmit}></input>
+      </form>
+    </div>
+  );
+};
 
 export default Home;
